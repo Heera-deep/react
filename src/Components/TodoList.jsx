7ba74d838704aa8842631d9dc0aac5f5af3a0dd1
unknown <https://gitlab.com/5KWebTech/menuverse.git>
2025-01-18 19:40:08 +0530

    Your commit message

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -93,7 +93,7 @@ const TodoList = () => {
                         key={index}
                         className="list-group-item d-flex justify-content-between align-items-center border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2"
                       >
-                        <div className="d-flex align-items-center">
+                        <div className="d-flex align-items-center kanfd">
                           {editingTask === task ? (
                             <div className="d-flex  NhV9ysu92XG@ANS">
                               <input
