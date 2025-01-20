e57678af9e5ef7228e3ab31bf52be1ebc63c0445
unknown <https://gitlab.com/5KWebTech/menuverse.git>
2025-01-20 23:33:56 +0530

    new code

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,62 @@
-import React from 'react';
-import { Routes, Route, NavLink } from 'react-router-dom';
-import MyCalendar from './Components/MyCalendar';
-import MyWebcam from './Components/MyWebcam';
-import ConfettiComponent from './Components/ConfettiComponent';
-import SwiperWithEffect from './Components/SwiperWithEffect';
-import FullForm from './Components/FullForm';
-import JoyrideTour from './Components/JoyrideTour';
-import Simple from './Components/Simple';
-import TodoList from './Components/TodoList';
-import Card from './Components/CardItems/card';
-import SingalCard from './Components/CardItems/SingalCard';
+import React from 'react'
+import { Routes, Route, NavLink } from "react-router-dom";
+import MyCalendar from "./Components/MyCalendar";
+import MyWebcam from "./Components/MyWebcam";
+import ConfettiComponent from "./Components/ConfettiComponent";
+import SwiperWithEffect from "./Components/SwiperWithEffect";
+import FullForm from "./Components/FullForm";
+import JoyrideTour from "./Components/JoyrideTour";
+import Simple from "./Components/Simple";
+import TodoList from "./Components/TodoList";
+import Card from "./Components/CardItems/card";
+import SingalCard from "./Components/CardItems/SingalCard";
 
 const App = () => {
   return (
     <>
-     <nav class="navbar navbar-expand-lg bg-body-tertiary  w-100 ">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav gap-5">
-        <a class="nav-link" href="#"> <NavLink to="/">Home</NavLink>      </a>
-        <a class="nav-link active" >        <NavLink to="/MyCalendar">MyCalendar</NavLink>
-        </a>
-        <a class="nav-link" href="#">        <NavLink to="/MyWebcam">MyWebcam</NavLink>
-        </a>
-        <a class="nav-link" href="#"> <NavLink to="/ConfettiComponent">ConfettiComponent</NavLink>      </a>
-        <a class="nav-link" href="#"> <NavLink to="/SwiperWithEffect">SwiperWithEffect</NavLink>      </a>
-        <a class="nav-link" href="#"> <NavLink to="/FullForm">FullForm</NavLink>      </a>
-        <a class="nav-link" href="#"> <NavLink to="/JoyrideTour">JoyrideTour</NavLink>      </a>
-        <a class="nav-link" href="#"> <NavLink to="/TodoList">TodoList</NavLink>      </a>
-      </div>
-    </div>
-  </div>
-</nav>
+    
+      <nav className="navbar navbar-expand-lg bg-body-tertiary  w-100 ">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav gap-5">
+                <NavLink className="nav-link" to="/">
+                  Home
+                </NavLink>
+                <NavLink className="nav-link" to="/MyCalendar">
+                  MyCalendar
+                </NavLink>
+                <NavLink className="nav-link" to="/MyWebcam">
+                  MyWebcam
+                </NavLink>
+                <NavLink className="nav-link" to="/ConfettiComponent">
+                  ConfettiComponent
+                </NavLink>
+                <NavLink className="nav-link" to="/SwiperWithEffect">
+                  SwiperWithEffect
+                </NavLink>
+                <NavLink className="nav-link" to="/FullForm">
+                  FullForm
+                </NavLink>
+                <NavLink className="nav-link" to="/JoyrideTour">
+                  JoyrideTour
+                </NavLink>
+                <NavLink className="nav-link" to="/TodoList">
+                  TodoList
+                </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
 
       {/* Routes Configuration */}
       <Routes>
